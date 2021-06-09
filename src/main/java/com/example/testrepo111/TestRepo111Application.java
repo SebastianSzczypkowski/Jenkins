f9f1b730fcfb0107170e45b91273139b55f720fd
SebastianSzczypkowski <szczsebastian146@gmail.com>
2021-06-09 09:16:19 +0200

    zmieniono plik main

--- src/main/java/com/example/testrepo111/TestRepo111Application.java
+++ src/main/java/com/example/testrepo111/TestRepo111Application.java
@@ -2,6 +2,7 @@ package com.example.testrepo111;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TestRepo111Application {
@@ -9,5 +10,12 @@ public class TestRepo111Application {
     public static void main(String[] args) {
         SpringApplication.run(TestRepo111Application.class, args);
     }
+
+    @GetMapping
+    public String get()
+    {
+        return "hello Jenkins";
+    }
+
 
 }
